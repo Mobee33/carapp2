4f829e789a623b7244c597781a6095c45fc69e46
Mubeen Bankur <mobee33@hotmail.com>
2022-08-27 15:03:13 +0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'gardens#index'
 
-  resources :gardens, only: [:index, :show, :new, :create] do
+  resources :gardens  do
     resources :plants, only: [:create]
   end
   resources :plants, only: [:destroy]
